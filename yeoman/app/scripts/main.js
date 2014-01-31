796a9bff7736fdb088ae7c91cd6b340883f0db7a
Scott Gilroy <scottjgilroy@gmail.com>
2014-01-31 11:10:41 -0500

    Initial implementation of chat using Firebase and AngularJS, based on the tutorial here https://www.firebase.com/tutorial/#tutorial/angular/0

--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+var app = angular.module("chia", ["firebase"]);
+
+app.controller('MyController', ['$scope', '$firebase', function($scope, $firebase) {
+    var ref = new Firebase("https://radiant-fire-2876.firebaseio.com/");
+    $scope.messages = $firebase(ref);
+    $scope.addMessage = function (e) {
+        if (e.keyCode != 13) return;
+        $scope.messages.$add({from: $scope.name, body: $scope.msg});
+        $scope.msg = "";
+    }
+}]);
