0845cf569ea6287e3d90ba58e492fb486ee95e20
Scott Gilroy <scottjgilroy@gmail.com>
2014-02-03 15:35:13 -0500

    Added Send button

--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -3,9 +3,14 @@ var app = angular.module("chia", ["firebase"]);
 app.controller('MyController', ['$scope', '$firebase', function($scope, $firebase) {
     var ref = new Firebase("https://radiant-fire-2876.firebaseio.com/");
     $scope.messages = $firebase(ref);
-    $scope.addMessage = function (e) {
-        if (e.keyCode != 13) return;
+
+    $scope.addMessage = function () {
         $scope.messages.$add({from: $scope.name, body: $scope.msg});
         $scope.msg = "";
+    };
+
+    $scope.messageKeyDownHandler = function (e) {
+        if (e.keyCode != 13) return;
+        this.addMessage();
     }
 }]);
