d0404ce1313da3b5a38e7e32f6554b70cfd79875
Scott Gilroy <scottjgilroy@gmail.com>
2014-02-08 14:44:54 -0500

    Added support for motor control via RFDuino

--- platforms/android/.staging/www/cordova_plugins.js
+++ platforms/android/.staging/www/cordova_plugins.js
@@ -6,12 +6,28 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/com.megster.cordova.rfduino/www/rfduino.js",
+        "id": "com.megster.cordova.rfduino.rfduino",
+        "clobbers": [
+            "rfduino"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.vibration": "0.3.6"
+    "org.apache.cordova.vibration": "0.3.6",
+    "com.megster.cordova.rfduino": "0.0.2",
+    "org.apache.cordova.device": "0.2.7"
 }
 // BOTTOM OF METADATA
 });
--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -1,47 +1,129 @@
 'use strict';
 
-var app = angular.module('chia', ['firebase', 'vr.directives.slider']);
+var app = angular.module('chia', [
+    'firebase',
+    'vr.directives.slider',
+    'fsCordova'
+]);
 
-app.controller('MyController', ['$scope', '$firebase', '$timeout', '$interval', function($scope, $firebase, $timeout, $interval) {
-    var messagesReference = new Firebase('https://radiant-fire-2876.firebaseio.com/messages');
-//    var vibrationReference = new Firebase('https://radiant-fire-2876.firebaseio.com/vibration');
-    var vibrationLevelReference = new Firebase('https://radiant-fire-2876.firebaseio.com/vibration/level');
-    $scope.messages = $firebase(messagesReference);
-//    $scope.vibration = $firebase(vibrationReference);
-    $scope.vibration = {};
-    $scope.vibration.level = $firebase(vibrationLevelReference);
-    $scope.messages.$on("change", function() {
-        console.log("A remote change was applied locally!");
-        $scope.vibrate();
+app.controller('MyController', ['$scope', '$firebase', '$timeout', '$interval', 'CordovaService', function($scope, $firebase, $timeout, $interval, CordovaService) {
+    function initialize() {
+        var messagesReference = new Firebase('https://radiant-fire-2876.firebaseio.com/messages');
+    //    var vibrationReference = new Firebase('https://radiant-fire-2876.firebaseio.com/vibration');
+        var vibrationLevelReference = new Firebase('https://radiant-fire-2876.firebaseio.com/vibration/level');
+        $scope.messages = $firebase(messagesReference);
+    //    $scope.vibration = $firebase(vibrationReference);
+        $scope.vibration = {};
+        $scope.vibration.level = $firebase(vibrationLevelReference);
+        $scope.messages.$on("change", function() {
+            console.log("A remote change was applied locally!");
+            $scope.vibrate();
+        });
+    
+        console.log("before bind $scope.vibration: " + JSON.stringify($scope.vibration));
+        $scope.vibration.level.$bind($scope, "vibration.level");
+        console.log("after bind $scope.vibration: " + JSON.stringify($scope.vibration));
+        
+        $scope.vibration.level.$on("change", function() {
+            console.log("$scope.vibration.level.$on change $scope.vibration: " + JSON.stringify($scope.vibration));
+        });
+        
+        $scope.$watch("vibration.level", function(newValue, oldValue) {
+            console.log("watch $scope.vibration: " + JSON.stringify($scope.vibration));
+            if (newValue > 0) {
+                $scope.startVibration();
+            } else {
+                $scope.stopVibration();
+            }
+            updateMotor(newValue);
+        });
+    }
+    
+    initialize();
+
+    CordovaService.ready.then(function() {
+        // Cordova is ready
+        $scope.refreshDeviceList();
     });
 
-//    $scope.vibration.$add({ level: 0 });
-//    $scope.vibration.$save("level");
-//    if (!$scope.vibration) {
-//        $scope.vibration = { level: 0 };
-//    }
-    
-//    if (!$scope.vibration.level) {
-//        $scope.vibration.$add({level: 0});
-//    }
-    
-    console.log("before bind $scope.vibration: " + JSON.stringify($scope.vibration));
-    $scope.vibration.level.$bind($scope, "vibration.level");
-    console.log("after bind $scope.vibration: " + JSON.stringify($scope.vibration));
-    
-    $scope.vibration.level.$on("change", function() {
-        console.log("$scope.vibration.level.$on change $scope.vibration: " + JSON.stringify($scope.vibration));
-    });
-    
-    $scope.$watch("vibration.level", function(newValue, oldValue) {
-//        $scope.vibration.$set({level: newValue});
-        console.log("watch $scope.vibration: " + JSON.stringify($scope.vibration));
-        if (newValue > 0) {
-            $scope.startVibration();
-        } else {
-            $scope.stopVibration();
+    function updateCordovaDebugInfo() {
+        var $rootHtml = angular.element(document.documentElement);
+
+        if (window.cordova) {
+            $rootHtml.addClass('device')
+                .addClass(window.device.model.replace(/\s+/g,'').toLowerCase())
+                .addClass(window.device.platform.replace(/\s+/g,'').toLowerCase());
         }
-    });
+
+/*
+        var element = document.getElementById('deviceProperties');
+        element.innerHTML = '<strong>Model:</strong> '    + window.device.model.replace(/\s+/g,'').toLowerCase()    + '<br>' +
+                            '<strong>Platform:</strong> ' + window.device.platform.replace(/\s+/g,'').toLowerCase() + '<br>' +
+                            '<strong>Pixel Density:</strong> ' +  window.devicePixelRatio + '<br>';
+*/
+
+        if (parseFloat(window.device.version) === 7.0) {
+            $rootHtml.addClass('ios7');
+        }
+    }
+
+    $scope.refreshDeviceList = function() {
+        // deviceList.innerHTML = ''; // empties the list
+        $scope.deviceList = [];
+        rfduino.discover(5, onDiscoverDevice, onError);
+    };
+        
+    function onDiscoverDevice(device) {
+/*
+        var listItem = document.createElement('li'),
+            html = '<b>' + device.name + '</b><br/>' +
+                'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
+                'Advertising: ' + device.advertising + '<br/>' +
+                device.uuid;
+
+        listItem.setAttribute('uuid', device.uuid);
+        listItem.innerHTML = html;
+        deviceList.appendChild(listItem);
+*/
+        
+        $scope.deviceList.push(device);
+    }
+        
+    $scope.connect = function(uuid) {
+        var onConnect = function() {
+                rfduino.onData(onData, onError);
+//                showDetailPage();
+            };
+
+        rfduino.connect(uuid, onConnect, onError);
+    };
+        
+    function onData(data) {
+        console.log(data);
+//        var celsius = arrayBufferToFloat(data),
+//            fahrenheit = celsius * 1.8 + 32;
+
+//        tempCelsius.innerHTML = celsius.toFixed(2);
+//        tempFahrenheit.innerHTML = fahrenheit.toFixed(2);
+    }
+        
+    function disconnect() {
+        rfduino.disconnect(showMainPage, onError);
+    }
+        
+    function showMainPage() {
+        mainPage.hidden = false;
+        detailPage.hidden = true;
+    }
+
+    function showDetailPage() {
+        mainPage.hidden = true;
+        detailPage.hidden = false;
+    }
+        
+    function onError(reason) {
+        alert(reason); // real apps should use notification.alert
+    }
     
     var stop;
     $scope.startVibration = function () {
@@ -58,6 +140,29 @@ app.controller('MyController', ['$scope', '$firebase', '$timeout', '$interval', 
             }, 100);
         }
     };
+    
+    function updateMotor(level) {
+        var levelNormalized = level * 255 / 100;
+        if (rfduino) {
+            rfduino.isConnected(
+                function() {
+                    console.log("RFduino is connected");
+
+                    var array = new Uint8Array(2);
+                    array[0] = 0; // index
+                    array[1] = levelNormalized;
+                    rfduino.write(levelNormalized.toString(), function() {
+                        console.log("RFduino write succeeded");
+                    }, function() {
+                        console.log("RFduino write failed");
+                    });
+                },
+                function() {
+                    console.log("RFduino is *not* connected");
+                }
+            );
+        }
+    }
 
     $scope.stopVibration = function () {
         if (angular.isDefined(stop)) {
